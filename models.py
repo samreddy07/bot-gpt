e25751c1a0260b053071140eca0021cee5213494
SAMARASIMHA REDDY <49056589+samreddy07@users.noreply.github.com>
2024-05-28 19:08:06 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,1 @@
-import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCywxL3BTdCMWt22qmZIxpOJVECFNbr02s")
-model = genai.GenerativeModel('gemini-1.5-pro-latest')
-model_video = genai.GenerativeModel(model_name="models/gemini-1.5-flash-latest")
-
-def get_gemini_response(input):
-  response = model.generate_content([input])
-  return response.text
