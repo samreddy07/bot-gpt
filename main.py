298aaec9086e25bafa512e1b018fec6b6af89d90
SAMARASIMHA REDDY <49056589+samreddy07@users.noreply.github.com>
2024-05-28 07:19:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import streamlit as st
 from models import get_gemini_response
 
-response = False
-prompt_tokens = 0
-completion_tokes = 0
-total_tokens_used = 0
-cost_of_response = 0
-
+response = ''
 
 def make_request(question_input: str):
     response = get_gemini_response(question_input)
@@ -34,21 +29,6 @@ else:
 
 if response:
     st.write("Response:")
-    st.write(response["choices"][0]["message"]["content"])
-
-    prompt_tokens = response["usage"]["prompt_tokens"]
-    completion_tokes = response["usage"]["completion_tokens"]
-    total_tokens_used = response["usage"]["total_tokens"]
-
-    cost_of_response = total_tokens_used * 0.000002
+    st.write(response)
 else:
     pass
-
-
-with st.sidebar:
-    st.title("Usage Stats:")
-    st.markdown("""---""")
-    st.write("Promt tokens used :", prompt_tokens)
-    st.write("Completion tokens used :", completion_tokes)
-    st.write("Total tokens used :", total_tokens_used)
-    st.write("Total cost of request: ${:.8f}".format(cost_of_response))
